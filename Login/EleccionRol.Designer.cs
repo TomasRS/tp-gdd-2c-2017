f236174500b8477be8da906d36f7ee93333d6f73
TomasRS <tomas_rs@hotmail.com>
2017-10-19 22:36:42 -0300

    Codigo Eleccion de Rol hecho, falta test

--- Login/EleccionRol.Designer.cs
+++ Login/EleccionRol.Designer.cs
@@ -89,6 +89,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "EleccionRol";
             this.Text = "Elección de Rol";
+            this.Load += new System.EventHandler(this.EleccionRol_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Login/EleccionRol.cs
+++ Login/EleccionRol.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using PagoAgilFrba.DataProvider;
+using PagoAgilFrba.Menu_Principal;
 
 namespace PagoAgilFrba.Login
 {
     public partial class EleccionRol : Form
     {
+        private SqlCommand command { get; set; }
+        private IList<SqlParameter> parametros = new List<SqlParameter>();
+
+        public Object SelectedItem { get; set; }
+
         public EleccionRol()
         {
             InitializeComponent();
@@ -21,13 +29,35 @@ namespace PagoAgilFrba.Login
         private void volverButton_Click(object sender, EventArgs e)
         {
             this.Close();
-            (new LoginForm()).Show();
+            new LoginForm().Show();
+        }
+
+        private void EleccionRol_Load(object sender, EventArgs e)
+        {
+            CargarRoles();
         }
 
         private void aceptarButton_Click(object sender, EventArgs e)
         {
-            /*Este boton lleva al menu principal el cual se cargara dependiendo del rol
-            que se tenga por defecto o bien si tiene muchos entonces el que eligio*/
+            String rolElegido = rolComboBox.SelectedValue.ToString();
+            UsuarioSesion.Usuario.rol = rolElegido;
+
+            this.Hide();
+            new MenuPrincipal().Show();
+            this.Close();
+        }
+
+        private void CargarRoles()
+        {
+            DataSet rolesUsuario = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@username", UsuarioSesion.usuario.nombre));
+            command = QueryBuilder.Instance.build("SELECT r.nombre from GAME_OF_CODE.Rol r, GAME_OF_CODE.Rol_por_Usuario ru WHERE r.estado_habilitacion = 1 AND (SELECT id_usuario FROM GAME_OF_CODE.Usuario WHERE username = @username) = ru.id_usuario AND r.id_rol = ru.id_rol ", parametros);
+            adapter.SelectCommand = command;
+            adapter.Fill(rolesUsuario, "Rol");
+            rolComboBox.DataSource = rolesUsuario.Tables[0].DefaultView;
+            rolComboBox.ValueMember = "nombre";
         }
     }
 }
